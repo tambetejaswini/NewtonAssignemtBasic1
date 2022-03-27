dc1e12456595701249a7c31d99b2931117f63f0a
tambetejaswini <tambetejaswini400@gmail.com>
2022-03-27 13:32:06 +0530

    commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <p>
+        Now I can render any React component on any DOM node I want using
+        ReactDOM.render
+      </p>
+    </div>
+  );
+}
